765b3c986eec8423726056e6ef6e287d8c57a5b6
Dmitriy <goglev37@gmail.com>
2024-02-04 21:25:26 +0300

    // создаем функцию для отрисовки любого (any ) плейлиста

--- lesson-01/player.js
+++ lesson-01/player.js
@@ -24,31 +24,59 @@ const playList = {
             artistName: '50 cent',
             trackCoverImgUrl: 'lesson-01/50Cent.png',
             trackFileUrl :'https://ds.cdn1.deliciouspeaches.com/get/cuts/1d/c9/1dc9bed60952bc86dc754ed6bca8f67d/47965582/50_Cent_-_In_Da_Club_b128f0d193.mp3',
-        }
+        },
     ]
 }
 // отрисовка playList. Главная картинка + заголовок
+//
+// const playListTitleElement = document.createElement('h1')
+// playListTitleElement.append(playList.playListInfo.title)
+// document.body.append(playListTitleElement)
+//
+// const playListCoverElement = document.createElement('Img')
+// playListCoverElement.src = playList.playListInfo.coverImgUrl
+// // можно изменить размер картинки
+// // playListCoverElement.style.width = '250px'
+// // playListCoverElement.style.height = '250px'
+// document.body.append(playListCoverElement)
+//
+// список треков
+//
+// const tracksListElement = document.createElement('ul')
+// for (let i = 0; i < playList.tracks.length; i++) {
+//     const trackElement = document.createElement('li')
+//     const trackCoverElement = document.createElement('Img')
+//     trackCoverElement.src = playList.tracks[i].trackCoverImgUrl
+//     trackElement.append(trackCoverElement)
+//     trackElement.append(playList.tracks[i].artistName + ': ' + playList.tracks[i].trackTitle)
+//     tracksListElement.append(trackElement)
+// }
+// document.body.append(tracksListElement)
 
-const playListTitleElement = document.createElement('h1')
-playListTitleElement.append(playList.playListInfo.title)
-document.body.append(playListTitleElement)
 
-const playListCoverElement = document.createElement('Img')
-playListCoverElement.src = playList.playListInfo.coverImgUrl
+// создаем функцию для отрисовки любого (any ) плейлиста
+function renderPlayList (anyPlayList){
+    const playListTitleElement = document.createElement('h1')
+    playListTitleElement.append(anyPlayList.playListInfo.title)
+    document.body.append(playListTitleElement)
+    const playListCoverElement = document.createElement('Img')
+    playListCoverElement.src = anyPlayList.playListInfo.coverImgUrl
 // можно изменить размер картинки
 // playListCoverElement.style.width = '250px'
 // playListCoverElement.style.height = '250px'
-document.body.append(playListCoverElement)
+    document.body.append(playListCoverElement)
 
-//список треков
+    //список треков
 
-const tracksListElement = document.createElement('ul')
-for (let i = 0; i < playList.tracks.length; i++) {
-    const trackElement = document.createElement('li')
-    const trackCoverElement = document.createElement('Img')
-    trackCoverElement.src = playList.tracks[i].trackCoverImgUrl
-    trackElement.append(trackCoverElement)
-    trackElement.append(playList.tracks[i].artistName + ': ' + playList.tracks[i].trackTitle)
-    tracksListElement.append(trackElement)
+    const tracksListElement = document.createElement('ul')
+    for (let i = 0; i < anyPlayList.tracks.length; i++) {
+        const trackElement = document.createElement('li')
+        const trackCoverElement = document.createElement('Img')
+        trackCoverElement.src = anyPlayList.tracks[i].trackCoverImgUrl
+        trackElement.append(trackCoverElement)
+        trackElement.append(anyPlayList.tracks[i].artistName + ': ' + anyPlayList.tracks[i].trackTitle)
+        tracksListElement.append(trackElement)
+    }
+    document.body.append(tracksListElement)
 }
-document.body.append(tracksListElement)
+renderPlayList(playList)
