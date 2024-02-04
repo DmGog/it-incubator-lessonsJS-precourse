a4c1e009e83fa779f3786b88a0ac1fa0021986e6
Dmitriy <goglev37@gmail.com>
2024-02-04 20:29:36 +0300

    отрисовка плейлиста : титульная картинка + список треков

--- lesson-01/player.js
+++ lesson-01/player.js
@@ -4,7 +4,7 @@ const playList = {
     playListId: '1',
     playListInfo: {
         title:'Hip-Hop Hits',
-        coverImgUrl:'./Title-List',
+        coverImgUrl:'lesson-01/Title-List.png',
         totalInfo:{
             totalTrackCount: 4,
             totalTrackDurationInSec: 733
@@ -15,15 +15,40 @@ const playList = {
             trackId: '10',
             trackTitle: 'Rap God',
             artistName: 'Eminem',
-            trackCoverImgUrl: './Eminem',
+            trackCoverImgUrl: 'lesson-01/Eminem.png',
             trackFileUrl :'https://ds.cdn4.deliciouspeaches.com/get/cuts/53/45/5345f1c087ec56d686a21e7013474e74/47964975/Eminem_-_Rap_God_b128f0d364.mp3',
         },
         {
             trackId: '11',
             trackTitle:'In da Club',
             artistName: '50 cent',
-            trackCoverImgUrl: './50Cent',
+            trackCoverImgUrl: 'lesson-01/50Cent.png',
             trackFileUrl :'https://ds.cdn1.deliciouspeaches.com/get/cuts/1d/c9/1dc9bed60952bc86dc754ed6bca8f67d/47965582/50_Cent_-_In_Da_Club_b128f0d193.mp3',
         }
     ]
 }
+// отрисовка playList. Главная картинка + заголовок
+
+const playListTitleElement = document.createElement('h1')
+playListTitleElement.append(playList.playListInfo.title)
+document.body.append(playListTitleElement)
+
+const playListCoverElement = document.createElement('Img')
+playListCoverElement.src = playList.playListInfo.coverImgUrl
+// можно изменить размер картинки
+// playListCoverElement.style.width = '250px'
+// playListCoverElement.style.height = '250px'
+document.body.append(playListCoverElement)
+
+//список треков
+
+const tracksListElement = document.createElement('ul')
+for (let i = 0; i < playList.tracks.length; i++) {
+    const trackElement = document.createElement('li')
+    const trackCoverElement = document.createElement('Img')
+    trackCoverElement.src = playList.tracks[i].trackCoverImgUrl
+    trackElement.append(trackCoverElement)
+    trackElement.append(playList.tracks[i].artistName + ': ' + playList.tracks[i].trackTitle)
+    tracksListElement.append(trackElement)
+}
+document.body.append(tracksListElement)
